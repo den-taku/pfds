4aededb5eeab3ef5207a203a50202577bff81bb6
dentaku <dentaku1412@gmail.com>
2021-08-26 01:56:10 +0900

    Implement BinarySearchTree

--- src/Persistent/src/set.rs
+++ src/Persistent/src/set.rs
@@ -1,14 +1,53 @@
 use std::rc::Rc;
+use Tree::*;
 
 pub trait Set {
     type Element;
 
-    fn empty() -> Self;
-    fn insert(self: Rc<Self>, element: Self::Element) -> Self;
-    fn member(&self, element: Self::Element) -> bool;
+    fn empty() -> Rc<Self>;
+    fn insert(self: Rc<Self>, element: Self::Element) -> Rc<Self>;
+    fn member(self: Rc<Self>, element: Self::Element) -> bool;
 }
 
-pub enum Tree<T> {
+pub enum Tree<T: PartialOrd + PartialEq> {
     Leaf,
     Node(Rc<Tree<T>>, T, Rc<Tree<T>>),
+}
+
+impl<T> Set for Tree<T>
+where
+    T: PartialOrd + PartialEq,
+{
+    type Element = T;
+    fn empty() -> Rc<Self> {
+        Rc::new(Leaf)
+    }
+    fn insert(self: Rc<Self>, element: Self::Element) -> Rc<Self> {
+        match &*self {
+            Leaf => Rc::new(Node(Set::empty(), element, Set::empty())),
+            Node(left, v, right) => {
+                if v == &element {
+                    self.clone()
+                } else if v < &element {
+                    right.clone().insert(element)
+                } else {
+                    left.clone().insert(element)
+                }
+            }
+        }
+    }
+    fn member(self: Rc<Self>, element: Self::Element) -> bool {
+        match &*self {
+            Leaf => false,
+            Node(left, v, right) => {
+                if v == &element {
+                    true
+                } else if v < &element {
+                    right.clone().member(element)
+                } else {
+                    left.clone().member(element)
+                }
+            }
+        }
+    }
 }
